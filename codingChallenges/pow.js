919204ee0e988f6bd6a5ee896352d6fe1051c75d
A-bhi <abhisheksinghbhati7@gmail.com>
2019-12-13 15:59:52 +0530

    modified

--- codingChallenges/pow.js
+++ codingChallenges/pow.js
@@ -62,3 +62,12 @@ function multiply( x,  res,  res_size) {
     } 
     main()
     
+    //above method shows some bugs...need to fix..
+
+    const myPow = (x, n) => {
+        if (n === 0) return 1;
+        if (n === 1) return x;
+        if (n < 0) return 1 / myPow(x, -n);
+        return (n & 1) === 0 ? myPow(x * x, n >>> 1) : x * myPow(x * x, (n - 1) >>> 1);
+      };
+      console.log(myPow(20,100))
